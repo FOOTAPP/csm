02f639dcc4a39a9994a25ee88f5984e5d43ed98c
admin <admin@Win7_1TB>
2017-05-27 21:39:07 +0200

    Clase BBDD_Conexion documentada y comentada

--- centanasProyecto/src/bbdd/BBDD_Conexion.java
+++ centanasProyecto/src/bbdd/BBDD_Conexion.java
@@ -11,41 +11,19 @@ import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
 /**
- * The Class BBDD_Conexion.
+ * Clase BBDD_Conexión.
  */
 public class BBDD_Conexion {
-
-	/** The base. */
 	private String base;
-	
-	/** The usuario. */
 	private String usuario;
-	
-	/** The pass. */
 	private String pass;
-	
-	/** The url. */
 	private String url;
-	
-	/** The c. */
 	protected Connection c;
-	
-	/** The prop. */
 	private Properties prop;
-	
-	/** The dbms. */
 	private String dbms;
-	
-	/** The driver. */
 	private String driver;
-	
-	/** The db name. */
 	private String dbName;
-	
-	/** The server name. */
 	private String serverName;
-	
-	/** The port number. */
 	private int portNumber;
 	/*
 	public BBDD_Conexion(String bbdd){		
@@ -57,7 +35,7 @@ public class BBDD_Conexion {
 	*/
 	
 	/**
-	 * Instantiates a new BBD D conexion.
+	 * Inicia una conexión de la BBDD.
 	 *
 	 * @param fileName no se usa
 	 *            
@@ -67,14 +45,14 @@ public class BBDD_Conexion {
 			this.prop = new Properties();
 			prop.loadFromXML(Files.newInputStream(Paths.get("xml\\mysql-properties.xml")));
 		
-			this.dbms = this.prop.getProperty("dbms");// nombre del sistema de datos "mysql"
-			this.driver = this.prop.getProperty("driver");// manejador de la base de datos
-			this.dbName = this.prop.getProperty("database_name");// nombre del esquema de la bbdd
-			this.usuario = this.prop.getProperty("user_name");// usuario con la que me concto a la bbdd
-			this.pass = this.prop.getProperty("password");// contraseña del ususario
-			this.serverName = this.prop.getProperty("server_name");// servidor donde esta desplegada la bbdd
-			this.portNumber = Integer.parseInt(this.prop.getProperty("port_number"));//puerto donde esta la base de datos
-			this.url = "jdbc:mysql://localhost/"+dbName; // es la conexion en si
+			this.dbms = this.prop.getProperty("dbms"); // nombre del sistema de datos "mysql"
+			this.driver = this.prop.getProperty("driver"); // manejador de la base de datos
+			this.dbName = this.prop.getProperty("database_name"); // nombre del esquema de la bbdd
+			this.usuario = this.prop.getProperty("user_name");  // usuario con la que me concto a la bbdd
+			this.pass = this.prop.getProperty("password");  // contraseña del ususario
+			this.serverName = this.prop.getProperty("server_name");  // servidor donde esta desplegada la bbdd
+			this.portNumber = Integer.parseInt(this.prop.getProperty("port_number"));  //puerto donde esta la base de datos
+			this.url = "jdbc:mysql://localhost/"+dbName;  // es la conexion en si
 		}
 		catch(FileNotFoundException e){
 			System.out.println("No se encuentra el archivo de configuración");  
@@ -88,17 +66,17 @@ public class BBDD_Conexion {
 	}
 	
 	/**
-	 * Abrir.
+	 * Abre.
 	 */
 	public void abrir(){
 		try{
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.jdbc.Driver"); // carga los drivers para conectar
 		}
 		catch (ClassNotFoundException e){
 			System.out.println(e.getMessage());
 		}
 		try{
-		 	c=DriverManager.getConnection(url,usuario,pass);
+		 	c=DriverManager.getConnection(url,usuario,pass); // Crea objeto tipo conección
 		}
 		catch (SQLException e ){
 			System.out.println(e.getMessage());
@@ -106,7 +84,7 @@ public class BBDD_Conexion {
 	}	
 	
 	/**
-	 * Cerrar.
+	 * Cierra.
 	 */
 	public void cerrar(){
 		try{
